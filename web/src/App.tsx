48291cc324ff48bd003aab7ff6356c13add67f1c
viitorxz <jvitorsantana@icloud.com>
2023-01-18 09:51:14 -0300

    Episódio 2 ✅

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,13 @@
-import { Habit } from "./components/Habit"
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={1} />
-      <Habit completed={2} />
-      <Habit completed={43} />
-      <Habit completed={35} />
-      <Habit completed={31} />
-      <Habit completed={311} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
