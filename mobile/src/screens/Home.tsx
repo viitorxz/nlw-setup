32c2869faeed5e33be7112acf915d7b8c8cff28f
viitorxz <jvitorsantana@icloud.com>
2023-01-18 10:58:18 -0300

    Episódio 2 ✅

---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,48 @@
+import { View, Text, ScrollView } from 'react-native';
+
+import { generateRangeBetweenDates } from '../utils/generate-range-between-dates';
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+import { DAY_SIZE, HabitDay } from '../components/HabitDay';
+import { Header } from '../components/Header';
+
+const datesFromYearStart = generateRangeBetweenDates();
+const minimumSummaryDatesSizes = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+
+export function Home() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 text-xl font-bold text-center mx-1 "
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 80 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 border-zinc-700 m-1 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
---
+++ mobile/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.tsx",
+    "./src/**/*.tsx"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#09090A",
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_900ExtraBold'
+      }
+    },
+  },
+  plugins: [],
+}
